4ed6667f11672cae6c0dc63719b83a9ba67d132b
yohira0616 <yukito.sys.so@gmail.com>
2016-11-22 01:51:21 +0900

    ApiClientHelperTestの実装

--- spec/teratail/common/api_client_helper_spec.rb
+++ spec/teratail/common/api_client_helper_spec.rb
@@ -1,9 +1,24 @@
 require 'rspec'
 require 'spec_helper'
 
-RSpec.describe '四則演算' do
-  # TODO: テストコードの記述
-  it '1 + 1 は 2 になること' do
-    expect(1 + 1).to eq 2
+RSpec.describe 'ApiClientHelperTest' do
+  before :all do
+    @target = Object.new
+    @target.extend ApiClientHelper
+  end
+
+  it '#make_request_headerのテスト__access_tokenが存在しない場合、空のインスタンスを返す' do
+    actual = @target.make_request_header('')
+    expect(actual.empty?).to eq true
+  end
+
+  it '#make_request_headerのテスト__access_tokenが存在する場合、Bearerトークンを返す' do
+    actual = @target.make_request_header('token_123456')
+    expect(actual['Authorization']).to eq 'Bearer token_123456'
+  end
+
+  it '#make_request_parameterのテスト' do
+    actual = @target.make_request_parameter(2, 15)
+    expect(actual).to eq '?page=2&limit=15'
   end
 end
