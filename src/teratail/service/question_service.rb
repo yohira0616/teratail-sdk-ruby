a1ab345880991febfa9143a92bd9f0c191fe99eb
yohira0616 <yukito.sys.so@gmail.com>
2016-11-21 23:54:44 +0900

    questionとusersのメソッド定義

--- src/teratail/service/question_service.rb
+++ src/teratail/service/question_service.rb
@@ -1,9 +1,11 @@
 require 'json'
 require 'rest-client'
 require 'yaml'
+require './common/api_client_helper.rb'
 
 class QuestionService
-  def initialize(access_token)
+  include ApiClientHelper
+  def initialize(access_token = '')
     @access_token = access_token
     @config = YAML.load_file('./config.yml')
   end
--- src/teratail/service/user_service.rb
+++ src/teratail/service/user_service.rb
@@ -1,10 +1,39 @@
 require 'json'
 require 'rest-client'
 require 'yaml'
+require './common/api_client_helper.rb'
 
 class UserService
-  def initialize(access_token)
+  include ApiClientHelper
+  def initialize(access_token = '')
     @access_token = access_token
     @config = YAML.load_file('./config.yml')
+  end
+
+  def find_all(page = 1, limit = 20)
+  end
+
+  def find_by_name(query, page = 1, limit = 20)
+  end
+
+  def find_one(display_name)
+  end
+
+  def find_my_tags(display_name, page = 1, limit = 20)
+  end
+
+  def find_clipped_question(display_name, page = 1, limit = 20)
+  end
+
+  def find_questions(display_name, page = 1, limit = 20)
+  end
+
+  def find_replies(display_name, page = 1, limit = 20)
+  end
+
+  def get_follower(display_name, page = 1, limit = 20)
+  end
+
+  def get_following(display_name, page = 1, limit = 20)
   end
 end
