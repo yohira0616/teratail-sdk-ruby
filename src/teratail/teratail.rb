74f422b7a3a80de9a0cd737273902ffa646222cb
yohira0616 <yukito.sys.so@gmail.com>
2016-11-21 23:30:24 +0900

    fix typo

--- src/teratail/teratail.rb
+++ src/teratail/teratail.rb
@@ -5,8 +5,8 @@ require './service/user_service.rb'
 class Teratail
   def initialize(access_token)
     @questionService = QuestionService.new(access_token)
-    @tagService = TagService.new(accessToken)
-    @userService = UserService.new(accessToken)
+    @tagService = TagService.new(access_token)
+    @userService = UserService.new(access_token)
   end
 
   def question
