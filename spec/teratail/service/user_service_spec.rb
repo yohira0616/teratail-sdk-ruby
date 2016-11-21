1c0548c604285dd94a991f96b83e2c437b015043
yohira0616 <yukito.sys.so@gmail.com>
2016-11-22 02:25:41 +0900

    usersServiceのテスト実装

--- spec/teratail/service/user_service_spec.rb
+++ spec/teratail/service/user_service_spec.rb
@@ -5,8 +5,51 @@ RSpec.describe 'UserServiceTest' do
   config = YAML.load_file('./spec/test_config.yml')
   access_token = config['access_token']
   target = UserService.new(access_token)
-  # TODO: テストコードの記述
-  it '1 + 1 は 2 になること' do
-    expect(1 + 1).to eq 2
+
+  display_name = 'teratail'
+
+  it '#find_allが200で返ってくること' do
+    actual = target.find_all
+    expect(actual['meta']['message']).to eq 'success'
+  end
+
+  it '#find_by_nameが200で返ってくること' do
+    actual = target.find_by_name(display_name)
+    expect(actual['meta']['message']).to eq 'success'
+  end
+
+  it '#find_oneが200で返ってくること' do
+    actual = target.find_one(display_name)
+    expect(actual['meta']['message']).to eq 'success'
+  end
+
+  it '#find_by_my_tagsが200で返ってくること' do
+    actual = target.find_my_tags(display_name)
+    expect(actual['meta']['message']).to eq 'success'
+  end
+
+  it '#find_clipped_questionが200で返ってくること' do
+    actual = target.find_clipped_question(display_name)
+    expect(actual['meta']['message']).to eq 'success'
+  end
+
+  it '#find_questionsが200で返ってくること' do
+    actual = target.find_questions(display_name)
+    expect(actual['meta']['message']).to eq 'success'
+  end
+
+  it '#find_repliesが200で返ってくること' do
+    actual = target.find_replies(display_name)
+    expect(actual['meta']['message']).to eq 'success'
+  end
+
+  it '#find_followerが200で返ってくること' do
+    actual = target.get_follower(display_name)
+    expect(actual['meta']['message']).to eq 'success'
+  end
+
+  it '#find_followingが200で返ってくること' do
+    actual = target.get_following(display_name)
+    expect(actual['meta']['message']).to eq 'success'
   end
 end
