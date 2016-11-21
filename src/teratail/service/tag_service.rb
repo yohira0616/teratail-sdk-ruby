11e513fa2880cb6f3ac6479ac6affd847497f1c8
yohira0616 <yukito.sys.so@gmail.com>
2016-11-21 23:12:30 +0900

    refactor for tag_service

--- src/teratail/service/tag_service.rb
+++ src/teratail/service/tag_service.rb
@@ -11,18 +11,21 @@ class TagService
     @config=YAML.load_file("./config.yml")
   end
 
-  def find_all(page=20,limit=1)
-    response = RestClient.get @config["teratail"]["host"]+'tags'+ make_request_parameter(page,limit)
+  def find_all(page=1,limit=20)
+    path= "#{@config["teratail"]["host"]}tags#{make_request_parameter(page,limit)}"
+    response = RestClient.get path
     JSON.parse response.to_s
   end
 
   def find_one(tag_name)
-    response = RestClient.get @config["teratail"]["host"]+'tags/'+tag_name
+    path= "#{@config["teratail"]["host"]}tags/#{tag_name}"
+    response = RestClient.get path
     JSON.parse response.to_s
   end
 
-  def find_by_tag_name(tag_name,page=20,limit=1)
-    response = RestClient.get @config["teratail"]["host"]+'tags/'+tag_name+'/questions'+make_request_parameter(page,limit)
+  def find_by_tag_name(tag_name,page=1,limit=20)
+    path= "#{@config["teratail"]["host"]}tags/#{tag_name}/questions#{make_request_parameter(page,limit)}"
+    response = RestClient.get path
     JSON.parse response.to_s
   end
 
