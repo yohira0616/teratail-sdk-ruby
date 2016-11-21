440a91f4d11bdac7e49deef680933ca6c4635d62
yohira0616 <yukito.sys.so@gmail.com>
2016-11-21 23:42:37 +0900

    リクエストヘッダーの読み込みを有効化

--- src/teratail/common/api_client_helper.rb
+++ src/teratail/common/api_client_helper.rb
@@ -2,4 +2,14 @@ module ApiClientHelper
   def make_request_parameter(page, limit)
     "?page=#{page}&limit=#{limit}"
   end
+
+  def make_request_header(access_token)
+    if access_token != ''
+      {
+        'Authorization' => "Bearer #{access_token}"
+      }
+    else
+      {}
+    end
+  end
 end
--- src/teratail/service/question_service.rb
+++ src/teratail/service/question_service.rb
@@ -4,7 +4,7 @@ require 'yaml'
 
 class QuestionService
   def initialize(access_token)
-    @accessToken = access_token
+    @access_token = access_token
     @config = YAML.load_file('./config.yml')
   end
 end
